8f4883a442fea3fc0b963363829cc31704f34dcc
nico <brun.nicolas21@gmail.com>
2018-04-10 14:58:18 +0200

    File type to embed file into archive/package

---
+++ cedar.go
@@ -0,0 +1,40 @@
+package cedar
+
+import (
+	"path/filepath"
+	"strings"
+)
+
+const etcDir = "etc/"
+
+type File struct {
+	Src      string `toml:"source"`
+	Dst      string `toml:"destination"`
+	Name     string `toml:"filename"`
+	Compress bool   `toml:"compress"`
+	Perm     int    `toml:"mode"`
+	Conf     bool   `toml:"conf"`
+}
+
+func (f File) String() string {
+	d, _ := filepath.Split(f.Dst)
+	return filepath.Join(d, f.Filename())
+}
+
+func (f File) Mode() int64 {
+	if f.Perm == 0 {
+		return 0644
+	}
+	return int64(f.Perm)
+}
+
+func (f File) Filename() string {
+	if f.Name == "" {
+		return filepath.Base(f.Src)
+	}
+	return f.Name
+}
+
+func IsConfFile(n string) bool {
+	return strings.Index(n, etcDir) >= 0
+}
