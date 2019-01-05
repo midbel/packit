00110e6d0826a6d0912e93db4146f047aab8f234
nico <brun.nicolas21@gmail.com>
2019-01-05 21:59:23 +0100

    line length smaller than max

--- rw/wrap.go
+++ rw/wrap.go
@@ -1,48 +1,60 @@
 package rw
 
 import (
-	"bytes"
-	"io"
-	"strings"
+  "bytes"
+  "io"
+  "strings"
 )
 
 const (
-	MinLineLength = 72
-	MaxLineLength = 80
+  MinLineLength = 72
+  MaxLineLength = 80
 )
 
 func WrapDefault(text string) string {
-	return Wrap(text, MinLineLength, MaxLineLength)
+  return Wrap(text, MinLineLength, MaxLineLength)
 }
 
 func Wrap(text string, min, max int) string {
-	r := strings.NewReader(text)
-	w, err := wrapText(r, min, max)
-	if err != nil {
-		return text
-	}
-	return w
+  r := strings.NewReader(text)
+  w, err := wrapText(r, min, max)
+  if err != nil {
+    return text
+  }
+  return w
 }
 
 func wrapText(r io.RuneReader, min, max int) (string, error) {
-	var body, line bytes.Buffer
-	for {
-		r, _, err := r.ReadRune()
-		if err == io.EOF || r == 0 {
-			break
-		}
-		if err != nil {
-			return "", err
-		}
-		if (r == ' ' && line.Len() >= min) || r == '\n' {
-			line.WriteRune('\n')
-			io.Copy(&body, &line)
-		} else {
-			line.WriteRune(r)
-		}
-	}
-	if line.Len() > 0 {
-		io.Copy(&body, &line)
-	}
-	return body.String(), nil
+  var (
+    body bytes.Buffer
+    line bytes.Buffer
+    last int
+  )
+  for {
+    r, _, err := r.ReadRune()
+    if err == io.EOF || r == 0 {
+       break
+    }
+    if err != nil {
+      return "", err
+    }
+    if (r == ' ' && line.Len() >= min) || r == '\n' {
+      if max > min && line.Len() >= max {
+        io.CopyN(&body, &line, int64(last))
+      } else {
+        io.Copy(&body, &line)
+      }
+      last = 0
+      body.WriteRune('\n')
+    } else {
+      line.WriteRune(r)
+      if r == ' ' {
+        last = line.Len()
+      }
+    }
+  }
+  if line.Len() > 0 {
+    io.Copy(&body, &line)
+  }
+  return body.String(), nil
 }
